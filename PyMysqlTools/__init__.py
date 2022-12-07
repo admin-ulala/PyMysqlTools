3cae6724585772f78d3f776ab17f0582397f6e15
Administrator <2713389652@qq.com>
2022-12-07 10:24:06 +0800

    update: 更新开发者日志, 提交新版本

--- PyMysqlTools/__init__.py
+++ PyMysqlTools/__init__.py
@@ -24,5 +24,5 @@ SOFTWARE.
 from PyMysqlTools.PyMysqlTools import connect
 from PyMysqlTools.PyMysqlTools import connect_pool
 
-__version__ = "0.5.17"
+__version__ = "0.5.18"
 name = "PyMysqlTools"
