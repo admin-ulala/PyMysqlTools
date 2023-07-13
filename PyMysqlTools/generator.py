35861d6f28a8de19b3124dd33189e9aed1c56b71
Administrator <2713389652@qq.com>
2023-07-13 16:59:17 +0800

    update: 补全类型提示

--- PyMysqlTools/generator.py
+++ PyMysqlTools/generator.py
@@ -7,18 +7,18 @@ class SqlGenerator:
         self.sql = ""
         self._clause_generator = ClauseGenerator()
 
-    def insert_one(self, tb_name, data: dict):
+    def insert_one(self, tb_name, data: dict) -> str:
         fields = self._clause_generator.get_fields(list(data.keys()))
         values = self._clause_generator.get_values(list(data.values()))
         self.sql = f"""INSERT INTO `{tb_name}` ({fields}) VALUES ({values})"""
         return self.sql.strip()
 
-    def delete_by(self, tb_name: str, condition=None):
+    def delete_by(self, tb_name: str, condition=None) -> str:
         where = self._clause_generator.build_where_clause(condition)
         self.sql = f"""DELETE FROM `{tb_name}` {where}"""
         return self.sql.strip()
 
-    def update_by(self, tb_name: str, data: dict, condition=None):
+    def update_by(self, tb_name: str, data: dict, condition=None) -> str:
         self.sql = f"""UPDATE `{tb_name}` """
         set_ = self._clause_generator.build_set_clause(data)
         self.sql += set_
@@ -27,7 +27,7 @@ class SqlGenerator:
             self.sql += where
         return self.sql.strip()
 
-    def find_by(self, tb_name: str, fields: list = None, condition=None):
+    def find_by(self, tb_name: str, fields: list = None, condition=None) -> str:
         if not fields:
             fields = ['*']
         fields = self._clause_generator.get_fields(fields)
@@ -66,25 +66,25 @@ class SqlGenerator:
         self.sql += " AND COLUMN_KEY = 'PRI'"
         return self.sql.strip()
 
-    def create_table(self, tb_name: str, schema):
+    def create_table(self, tb_name: str, schema) -> str:
         schema = self._clause_generator.get_schema(schema)
         self.sql = f"""
         CREATE TABLE `{tb_name}` (\n{schema}\n) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci
         """
         return self.sql.strip()
 
-    def create_table_not_exists(self, tb_name: str, schema):
+    def create_table_not_exists(self, tb_name: str, schema) -> str:
         schema = self._clause_generator.get_schema(schema)
         self.sql = f"""
         CREATE TABLE IF NOT EXISTS `{tb_name}` (\n{schema}\n) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci
         """
         return self.sql.strip()
 
-    def truncate_table(self, tb_name: str):
+    def truncate_table(self, tb_name: str) -> str:
         self.sql = f"""TRUNCATE TABLE `{tb_name}`"""
         return self.sql.strip()
 
-    def delete_table(self, tb_name: str):
+    def delete_table(self, tb_name: str) -> str:
         self.sql = f"""DELETE TABLE `{tb_name}`"""
         return self.sql.strip()
 
@@ -95,18 +95,18 @@ class ClauseGenerator:
         self.clause = ""
 
     @staticmethod
-    def get_fields(fields):
+    def get_fields(fields) -> str:
         if isinstance(fields, dict):
             fields = list(fields.values())
         return f"""{", ".join([f"`{i}`" if '*' not in i else f"{i}" for i in fields])}"""
 
     @staticmethod
-    def get_values(values):
+    def get_values(values) -> str:
         if isinstance(values, dict):
             values = list(values.values())
         return f"""{", ".join([f"%s" for _ in values])}"""
 
-    def get_schema(self, data):
+    def get_schema(self, data) -> str:
         schema = []
         if isinstance(data, list):
             for i in data:
@@ -130,7 +130,7 @@ class ClauseGenerator:
 
         raise TypeMismatchError("'schema' 只能是 list/dict 类型")
 
-    def build_where_clause(self, condition):
+    def build_where_clause(self, condition) -> str:
         condition_str = ''
 
         if not condition:
@@ -151,7 +151,7 @@ class ClauseGenerator:
         self.clause = f"""WHERE {condition_str}"""
         return self.clause
 
-    def build_set_clause(self, data: dict):
+    def build_set_clause(self, data: dict) -> str:
         temp = []
         for k, v in data.items():
             temp.append(f"""`{k}`=%s""")
@@ -169,7 +169,7 @@ class ClauseGenerator:
     def build_order_clause(self):
         pass
 
-    def build_limit_clause(self, index: int, step: int = None):
+    def build_limit_clause(self, index: int, step: int = None) -> str:
         self.clause = f""" LIMIT {index}"""
         if step:
             self.clause += f""", {step}"""
